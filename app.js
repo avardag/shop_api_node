37c7069980d500e01c0eb6666dfa21d3b2727746
Dagestan05 <steklofan@gmail.com>
2018-09-10 04:41:03 +0200

    added route err handling

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-const express = require('express')
-
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const productsRoutes = require("./api/routes/products")
-const ordersRoutes = require("./api/routes/orders")
+//Route imports
+const productsRoutes = require("./api/routes/products");
+const ordersRoutes = require("./api/routes/orders");
+
+app.use(morgan('dev'));
 
 
 // ROUTES
-app.use("/products", productsRoutes)
-app.use("/orders", ordersRoutes)
+app.use("/products", productsRoutes);
+app.use("/orders", ordersRoutes);
+
+// catch allroute / Error Handling
+app.use((req, res, next)=>{
+  const error = new Error('Not Found!!!'); //send err msg / set err.message
+  error.status = 404; // send err status
+  next(error); // go to next with error as arg
+})
+//repond with error messages
+app.use((err, req, res, next)=>{
+  res.status(err.status || 500);
+  res.json({
+    error:{
+      message: err.message
+    }
+  })
+})
 
 module.exports = app;
